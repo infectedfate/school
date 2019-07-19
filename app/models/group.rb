a1f443b3dc64d2f77db12dfc5b7f8641c09acbf6
Malinin Mikhail <infectedfate@gmail.com>
2019-07-19 17:55:54 +0300

    fix error in validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   has_many :students
 
-  validates :number, :character, prescense: true
+  validates :number, :character, presence: true
 
 end
--- app/models/perfomance.rb
+++ app/models/perfomance.rb
@@ -2,5 +2,5 @@ class Perfomance < ApplicationRecord
   belongs_to :student
   has_many :subjects
 
-  validates :subject, :mark, prescense: true
+  validates :subject, :mark, presence: true
 end
