cf461de18a20fe824dc211ff16db99266e8be5c3
Malinin Mikhail <infectedfate@gmail.com>
2019-07-23 18:19:48 +0300

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-SUBJECTS = %w[math, programming, world, russian, english]
+SUBJECTS = %w[math programming world russian english]
 
 teachers = Teacher.create([
   {name: "Petr Aleksandrovich", email: "klavaklava@klava.com", password: "asdawawds", password_confirmation: "asdawawds"}
